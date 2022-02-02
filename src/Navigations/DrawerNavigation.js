67a68ddb8f155815338eadff380f835e112abab1
Yağmur Doğan <yagmurdogan00@gmail.com>
2022-02-02 22:26:34 +0300

    for main control

--- src/Navigations/DrawerNavigation.js
+++ src/Navigations/DrawerNavigation.js
@@ -2,6 +2,8 @@ import * as React from "react";
 import { createDrawerNavigator } from "@react-navigation/drawer";
 import Home from "../Screens/Home";
 import Details from "../Screens/Details";
+import Profile from "../Screens/Profile";
+import Settings from "../Screens/Settings";
 
 export default function DrawerNavigation() {
   const Drawer = createDrawerNavigator();
@@ -10,6 +12,8 @@ export default function DrawerNavigation() {
     <Drawer.Navigator initialRouteName="Home">
       <Drawer.Screen name="Home" component={Home} />
       <Drawer.Screen name="Details" component={Details} />
+      <Drawer.Screen name="Profile" component={Profile} />
+      <Drawer.Screen name="Settings" component={Settings} />
     </Drawer.Navigator>
   );
 }
