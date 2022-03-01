86044100d9f867fe6ca757948dce90e3dbc96e0b
ercan unal <unalercann@gmail.com>
2022-03-01 16:19:27 +0300

    update tab navigator

--- src/Screens/Settings.js
+++ src/Screens/Settings.js
@@ -4,7 +4,7 @@ import { View, Text } from "react-native";
 export default function Settings() {
   return (
     <View>
-      <Text>Settingasdadsasds</Text>
+      <Text>Settings</Text>
     </View>
   );
 }
