7b0f53c002b1533d434d583580dbd8b487575e88
ercan unal <unalercann@gmail.com>
2022-03-01 16:19:07 +0300

    test

--- src/Screens/Settings.js
+++ src/Screens/Settings.js
@@ -4,7 +4,7 @@ import { View, Text } from "react-native";
 export default function Settings() {
   return (
     <View>
-      <Text>Settings</Text>
+      <Text>Settingasdadsasds</Text>
     </View>
   );
 }
